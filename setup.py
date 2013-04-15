160bfa6a19317649a41b111d3958c670dfd1517c
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-04-15 15:18:16 +0300

    Updated for Plone-4.3.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=open("README.rst").read(),
     classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7"],
