0b86d88322ba2b87c9cddaaa8929d21012379701
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-04-15 15:25:11 +0300

    Release 0.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='abita.utils',
-    version='0.6',
+    version='0.7',
     description='Utilities for Plone.',
     long_description=open("README.rst").read(),
     classifiers=[
